41dcfd003f70278800f32aba5bdc1c94a526cc04
gkraud4135 <77388722+gkraud4135@users.noreply.github.com>
2021-08-03 15:36:54 +0900

    Add files via upload

---
+++ RelativeLayout/src/main/java/com/example/hello04relativelayouttest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hello04relativelayouttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v){
+        setContentView(R.layout.normal);
+    }
+
+}
